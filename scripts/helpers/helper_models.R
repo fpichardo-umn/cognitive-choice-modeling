fc7c9c55c4781b60ba2e1bfd9ccaddff5a59d579
Felix Pichardo <fpichard@umn.edu>
2025-10-20 06:52:52 -0500

    fix ev rd model

--- scripts/helpers/helper_models.R
+++ scripts/helpers/helper_models.R
@@ -748,7 +748,7 @@ get_igt_defaults = function() {
   
   models[["igt_hier_ev_rd_b1p2"]] <- list(
     data = unique(c(data_types$with_rt_igt, data_types$basic_hier_igt)),
-    params = setdiff(c(param_sets$rd_b1p2, param_sets$ev), c("con", "drift_con")),
+    params = setdiff(c(param_sets$rd_b1p2, param_sets$ev), c("con", "urgency")),
     non_pr_params = NULL,
     exclude_params = NULL
   )
