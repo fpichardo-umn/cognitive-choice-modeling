cd9caf0e02455b246264199f691a84adadb37657
Felix Pichardo <fpichard@umn.edu>
2025-11-15 17:24:36 -0600

    fix sim ddm

--- scripts/simulation/models/igt_mod/experimental/igt_mod_ev_ddm_b1p2_model.R
+++ scripts/simulation/models/igt_mod/experimental/igt_mod_ev_ddm_b1p2_model.R
@@ -56,7 +56,7 @@ igt_modEVDDMB1P2Model <- R6::R6Class("igt_modEVDDMB1P2Model",
                                        ev_history[t,] <- self$ev
                                        
                                        # Calculate drift rate based on expected value
-                                       sensitivity <- (3^con) - 1
+                                       sensitivity <- (3^drift_con) - 1
                                        drift_rate <- sensitivity * self$ev[shown_deck]
                                        
                                        # Determine boundary and tau based on block
@@ -152,7 +152,7 @@ igt_modEVDDMB1P2Model <- R6::R6Class("igt_modEVDDMB1P2Model",
                                        shown_deck <- as.numeric(deck_shown[t])
                                        
                                        # Calculate drift rate based on expected value
-                                       sensitivity <- (3^parameters$con) - 1
+                                       sensitivity <- (3^parameters$drift_con) - 1
                                        drift_rate <- sensitivity * self$ev[shown_deck]
                                        
                                        # Determine boundary and tau based on block
